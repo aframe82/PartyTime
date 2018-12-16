e25d604ed2a6db9e8a0e3e2a0b9810bd7398dc47
Adam Farah <adam@rangeroom.com>
2018-12-16 18:48:28 +0000

    Scaffold readFile

---
+++ partytime.js
@@ -0,0 +1,16 @@
+/**
+ * PartyTime.js
+ * There's a party at Intercom. All customers within 100km are invited. 
+ * Bring your party shirts.
+ */
+
+/**
+* Step 1 - The invitees
+* Read a file that has the customer name, id, and lat/lng pairs.
+* (downloaded locally for brevity)
+*/
+const fs = require('fs')
+const readFile = async (file) => {
+  // Does the file exist (relative to project dir)
+  let exists = fs.existsSync()
+}
