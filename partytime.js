89c92880e08b188246e350a132be7d77ebc68bf6
Adam Farah <adam@rangeroom.com>
2018-12-17 18:40:45 +0000

    Add more tests

--- partytime.js
+++ partytime.js
@@ -5,27 +5,48 @@
  */
 
 /**
+ * CONSTANTS 
+ */
+const RADIUS_KM = 6371 // Earth Radius
+const MAX_DISTANCE = 100 // Max distance in KM of invitees
+const dubsOffice = { // Dublin Office coords
+  latitude: 53.339428,
+  longitude: -6.257664
+}
+
+/**
 * Step 1 - The invitees
 * Read a file that has the customer name, id, and lat/lng pairs.
 * (downloaded locally for brevity)
 */
-const readFile = async (file) => {
-  const fs = require('fs')
-
-  // Does the file exist (relative to project dir)
-  const fileExists = fs.existsSync(file)
-  if (!fileExists) {
-    throw "File does not exist. No parties without people! Aborting."
-  }
-
-  // Let this function return a Promise
+const readFile = (file) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        return reject(err)
-      }
-      // Turn the Buffer data into a string
-      resolve(data.toString())
+    const fs = require('fs')
+    const readline = require('readline')
+
+    // Does the file exist (relative to project dir)
+    const fileExists = fs.existsSync(file)
+    if (!fileExists) {
+      throw 'File does not exist. No parties without people! Aborting.'
+    }
+
+    const fileStream = fs.createReadStream(file)
+    const rl = readline.createInterface({ input: fileStream, crlfDelay: Infinity })
+
+    let lines = []
+    rl.on('line', line => {
+      // read a line at a time of the file (so we can construct a JSON object later)
+      lines.push(JSON.parse(line))
+    })
+
+    rl.on('close', () => {
+      // return the promise when we've done with the file read
+      return resolve(lines)
+    })
+
+    rl.on('error', error => {
+      // crash and burn if we hit an error reading the file
+      return reject(error)
     })
   })
 }
@@ -34,8 +55,13 @@ const _degressToRadian = (degrees) => {
   return (degrees * Math.PI / 180.0)
 }
 
-const RADIUS_KM = 6371
-
+/**
+ * Return the distance in KM of the lat/lng pairs
+ * @param {Number} _lat1 
+ * @param {Number} _lng1 
+ * @param {Number} _lat2 
+ * @param {Number} _lng2 
+ */
 const distanceInKilometres = (_lat1, _lng1, _lat2, _lng2) => {
   const lat1 = _degressToRadian(_lat1)
   const lng1 = _degressToRadian(_lng1)
@@ -44,34 +70,78 @@ const distanceInKilometres = (_lat1, _lng1, _lat2, _lng2) => {
 
   const diffLat = lat2 - lat1
   const diffLng = lng2 - lng1
-  console.log(diffLat)
-  console.log(diffLng)
 
-  const a = Math.pow(Math.sin(diffLat/2),2 ) + 
-            // Math.sin(diffLat/2) * Math.sin(diffLat/2) + 
-            Math.cos(lat1) * Math.cos(lat2) *
-            Math.pow(Math.sin(diffLng / 2), 2)
-            // Math.sin(diffLng / 2) * Math.sin(diffLng/2)
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-  console.log(a)
-  console.log(c)
-  console.log(RADIUS_KM * c)
+  const a = Math.pow(Math.sin(diffLat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) *
+    Math.pow(Math.sin(diffLng / 2), 2)
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
   return RADIUS_KM * c
 }
 
-distanceInKilometres(51.511461, 0.063697, 53.339428, -6.257664)
+/**
+ * Return an Array of entries with distances in km
+ * @param {Object} contents 
+ */
+const mapContentsToEntriesWithDistance = (contents) => {
+  // quick type check
+  if (!contents instanceof Array) {
+    return Promise.reject('mapContents.. not received an array')
+  }
+  // convert distances and add distance in km to each customer entry
+  contents.map(entry => entry.distance_km = distanceInKilometres(entry.latitude, entry.longitude, dubsOffice.latitude, dubsOffice.longitude))
+  return Promise.resolve(contents)
+}
 
+/**
+ * Return an Array of entries that do not exceed the max distance
+ * @param {Number} maxDistance 
+ * @param {Array} distances 
+ */
+const distancesLessThanKm = (maxDistance, distances) => {
+  if (maxDistance < 1) {
+    return Promise.reject('Max distance must be greater than 0')
+  }
+
+  const filteredDistances = distances.filter(distance => distance.distance_km <= maxDistance)
+  return Promise.resolve(filteredDistances)
+}
+
+// Read the file from the project dir
+readFile('./customers.txt')
+  .then(contents => {
+    // Make sure it's the right kind of object before moving forward
+    if (typeof contents !== 'object') {
+      return Promise.reject('Not an object')
+    }
+
+    return Promise.resolve(contents)
+  })
+  // send this object to have it's distances calculated
+  .then(contents => mapContentsToEntriesWithDistance(contents))
+  // grab the distances that we want
+  .then(entries => distancesLessThanKm(MAX_DISTANCE, entries))
+  // return the party people
+  .then(closeEntries => {
+    let partyList = closeEntries.sort((a, b) => a.user_id - b.user_id).map(customer => {
+      return {
+        userId: customer.user_id,
+        customerName: customer.name,
+        distance: customer.distance_km,
+        distanceString: `${customer.distance_km.toFixed(1)} KM`
+      }
+    })
+    // show some magic
+    console.log(partyList)
+  })
+  .catch(err => {
+    throw err
+  })
 
 module.exports = {
   _degressToRadian,
   distanceInKilometres,
-  readFile
-}
-
-// readFile('./customers.txt')
-//   .then(customers => {
-//     console.log(customers)
-//   })
-//   .catch(err => {
-//     throw err
-//   })
+  mapContentsToEntriesWithDistance,
+  distancesLessThanKm,
+readFile}
--- partytime.test.js
+++ partytime.test.js
@@ -5,10 +5,32 @@ test('Customers file exists', () => {
   expect(fs.existsSync('./customers.txt')).toBeTruthy()
 })
 
+test('Imported file has correct properties for each entry', async () => {
+  let entries = await partytime.readFile('./customers.txt')
+  entries.forEach(entry => {
+    expect(entry).toHaveProperty('latitude')
+    expect(entry).toHaveProperty('longitude')
+    expect(entry).toHaveProperty('user_id')
+    expect(entry).toHaveProperty('name')
+  })
+})
+
 test('_degreesToRadian returns ~0.01745329252 for 1 degree', () => {
   expect(partytime._degressToRadian(1)).toBeCloseTo(0.01745329252)
 })
 
-test('distanceInKilometres Returns a number', () => {
-  expect(partytime.distanceInKilometres(1, 1, 1, 1)).toBe(100)
+test('distanceInKilometres is zero for same place', () => {
+  expect(partytime.distanceInKilometres(53.339428, -6.257664, 53.339428, -6.257664)).toBe(0)
+})
+
+test('mapContentsToEntriesWithDistance returns correct distance', async () => {
+  let entries = [{"latitude": "52.986375", "user_id": 12, "name": "Christina McArdle", "longitude": "-6.043701"}]
+  let mappedContents = await partytime.mapContentsToEntriesWithDistance(entries)
+  expect(mappedContents[0]).toHaveProperty('distance_km')
+  expect(mappedContents[0].distance_km).toBeCloseTo(41.768725500836844)
+})
+
+test('distancesLessThanKm rejects if max distance is 0', async () => {
+  expect.assertions(1)
+  expect(partytime.distancesLessThanKm(0, [])).rejects.toEqual("Max distance must be greater than 0")
 })
